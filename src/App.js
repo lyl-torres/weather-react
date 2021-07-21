e1526654184a013f7ec890c27be296a8d70105eb
Liliana Blattner <lilian14torres@gmail.com>
2021-07-20 18:52:52 -0700

    just testing something

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import Weather from "./Weather";
+// import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div><Weather /></div>
+        <span>Hola Mundo</span>
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,78 +1,78 @@
-import React from "react";
+// import React from "react";
 
-export default function Weather(){
-    return (
-        <div className="App">
-        <div className="weather-app-wrapper">
-            <div className="weather-app">
-            <form id="search-form" className="search-form">
-                <div className="row">
-                <div className="col-9">
-                    <input
-                    type="search"
-                    placeholder="Type a city.."
-                    class="form-control"
-                    id="city-input"
-                    autofocus="on"
-                    autocomplete="off"
-                    />
-                </div>
-                <div class="col-3">
-                    <input
-                    type="submit"
-                    value="Search"
-                    className="form-control btn btn-primary w-100"
-                    />
-                </div>
-                </div>
-            </form>
-            <hr />
-            <div className="overview">
-                <div className="row">
-                <div className="col-6">
-                    <h1 id="city">Mexico City</h1>
-                    <ul>
-                    <li>
-                        <span id="date"></span>
-                    </li>
-                    <li id="description"></li>
-                    <ul>
-                        <li>
-                        Humidity:{" "}
-                        <strong>
-                            <span id="humidity"></span>%
-                        </strong>
-                        </li>
-                        <li>
-                        Wind:{" "}
-                        <strong>
-                            <span id="wind"></span> km/h{" "}
-                        </strong>
-                        </li>
-                    </ul>
-                    </ul>
-                </div>
-                <div className="col-6">
-                    <div className="weather-temperature justify-content-end">
-                    <img src="" alt="Clear" id="icon" class="float-right" />
-                    <strong id="temperature"></strong>
-                    <span className="units">
-                        <a href="#" id="celsius-link" className="active">
-                        °C{" "}
-                        </a>
-                        |
-                        <a href="#" id="fahrenheit-link">
-                        °F
-                        </a>
-                    </span>
-                    </div>
-                </div>
-                <hr />
-                <div className="weather-forecast" id="forecast"></div>
-                <hr />
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
-  )}
+// export default function Weather(){
+//     return (
+//         <div className="App">
+//         <div className="weather-app-wrapper">
+//             <div className="weather-app">
+//             <form id="search-form" className="search-form">
+//                 <div className="row">
+//                 <div className="col-9">
+//                     <input
+//                     type="search"
+//                     placeholder="Type a city.."
+//                     class="form-control"
+//                     id="city-input"
+//                     autofocus="on"
+//                     autocomplete="off"
+//                     />
+//                 </div>
+//                 <div class="col-3">
+//                     <input
+//                     type="submit"
+//                     value="Search"
+//                     className="form-control btn btn-primary w-100"
+//                     />
+//                 </div>
+//                 </div>
+//             </form>
+//             <hr />
+//             <div className="overview">
+//                 <div className="row">
+//                 <div className="col-6">
+//                     <h1 id="city">Mexico City</h1>
+//                     <ul>
+//                     <li>
+//                         <span id="date"></span>
+//                     </li>
+//                     <li id="description"></li>
+//                     <ul>
+//                         <li>
+//                         Humidity:{" "}
+//                         <strong>
+//                             <span id="humidity"></span>%
+//                         </strong>
+//                         </li>
+//                         <li>
+//                         Wind:{" "}
+//                         <strong>
+//                             <span id="wind"></span> km/h{" "}
+//                         </strong>
+//                         </li>
+//                     </ul>
+//                     </ul>
+//                 </div>
+//                 <div className="col-6">
+//                     <div className="weather-temperature justify-content-end">
+//                     <img src="" alt="Clear" id="icon" class="float-right" />
+//                     <strong id="temperature"></strong>
+//                     <span className="units">
+//                         <a href="#" id="celsius-link" className="active">
+//                         °C{" "}
+//                         </a>
+//                         |
+//                         <a href="#" id="fahrenheit-link">
+//                         °F
+//                         </a>
+//                     </span>
+//                     </div>
+//                 </div>
+//                 <hr />
+//                 <div className="weather-forecast" id="forecast"></div>
+//                 <hr />
+//                 </div>
+//             </div>
+//             </div>
+//         </div>
+//         </div>
+//   )}
