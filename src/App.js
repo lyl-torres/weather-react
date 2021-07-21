0f6bc0c2643842fddc5fc7f347fde2c9f1cfc105
Liliana Blattner <lilian14torres@gmail.com>
2021-07-20 19:42:14 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       </header>
     </div>
   );
+  <footer className="App-footer"><a href="https://github.com/lyl-torres/weather-react">Open source code</a>, by Liliana Torres.</footer>
 }
 
 export default App;
