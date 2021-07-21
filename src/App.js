9273b7ec820370a8845b6cc3ccd88cdcb7c85e12
Liliana Blattner <lilian14torres@gmail.com>
2021-07-20 19:00:01 -0700

    hola mundo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
-import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Weather />
+        <span>Hola Mundo</span>
       </header>
     </div>
   );
