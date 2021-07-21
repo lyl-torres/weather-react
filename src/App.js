cc97a4a5a964039b4660568d841ed8f21330772b
Liliana Blattner <lilian14torres@gmail.com>
2021-07-20 17:27:22 -0700

    imported code from sandbox to vs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Hola</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div><Weather /></div>
       </header>
     </div>
   );
