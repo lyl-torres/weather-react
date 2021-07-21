0dc66e7779ca2eaebe412ce201c14756454d890c
Liliana Blattner <lilian14torres@gmail.com>
2021-07-20 19:02:46 -0700

    fixed the deployement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span>Hola Mundo</span>
+      <Weather />
       </header>
     </div>
   );
