fc168e22487a975c746c25286537b4be690e7407
Liliana Blattner <lilian14torres@gmail.com>
2021-07-25 13:58:16 -0700

    added a state, worked with the object to extract the information, added api key and api url, changed ids for classNames

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css";
+
 export default function Weather(){
+    const [ready, setReady] = useState(false)
+    const [weatherData, setweatherData] = useState({});
+  function handleResponse(response) {
+    setweatherData({
+temperature: (Math.round(response.data.main.temp)),
+description: response.data.weather[0].description,
+humidity: response.data.main.humidity,
+wind: response.data.main.wind,
+city: response.data.name,
+    });
+    
+    setReady(true);
+  }
+
+  if (ready){
     return (
         <div className="Weather">
         <div className="weather-app-wrapper">
             <div className="weather-app">
-            <form id="search-form" className="search-form">
+            <form className="search-form">
                 <div className="row">
                 <div className="col-9">
                     <input
                     type="search"
-                    placeholder="Type a city.."
+                    placeholder="Enter  a city.."
                     className="form-control"
-                    id="city-input"
                     autoFocus="on"
                     autoComplete="off"
                     />
@@ -30,29 +46,28 @@ export default function Weather(){
             <div className="overview">
                 <div className="row">
                 <div className="col-6">
-                    <h1 id="city">Mykonos</h1>
+                    <h1>Mykonos</h1>
                     <ul>
                     <li>
-                        <span id="date"></span>
+                        <span className="date"></span>
                     </li>
-                    <li id="description"></li>
                     <ul>
                         <li>
                         Humidity:{" "}
                         <strong>
-                            <span id="humidity"></span>%
+                            <span className="humidity">{weatherData.humidity}</span>%
                         </strong>
                         </li>
                         <li>
                         Wind:{" "}
                         <strong>
-                            <span id="wind"></span> km/h{" "}
+                            <span className="wind">{weatherData.wind}</span> km/h{" "}
                         </strong>
                         </li>
                         <li>
                         Precipitation:{" "}
                         <strong>
-                            <span id="precipitation"></span> %{" "}
+                            <span className="precipitation"></span> %{" "}
                         </strong>
                         </li>
                     </ul>
@@ -60,25 +75,35 @@ export default function Weather(){
                 </div>
                 <div className="col-6">
                     <div className="weather-temperature justify-content-end">
-                    <img src="" alt="Clear" id="icon" className="float-right" />
-                    <strong id="temperature"></strong>
+                    <img src="" alt="Clear" className="icon" className="float-right" />
+                    <div className="text-capitalize">{weatherData.description}</div>
+                    <strong iclassName="temperature">{weatherData.temperature}</strong>
                     <span className="units">
-                        <a href="www.google.com" id="celsius-link" className="active">
+                        <a href="www.google.com" className="celsius-link" className="active">
                         °C{" "}
                         </a>
                         |
-                        <a href="www.google.com" id="fahrenheit-link">
+                        <a href="www.google.com" className="fahrenheit-link">
                         °F
                         </a>
                     </span>
                     </div>
                 </div>
                 <hr />
-                <div className="weather-forecast" id="forecast"></div>
+                <div className="weather-forecast" className="forecast"></div>
                 <hr />
                 </div>
             </div>
             </div>
         </div>
         </div>
-  )}
+  );
+} else {
+ const apiKey = "ad95ba85cf6afff9b76905b9243ff9af";
+ let city = "Mykonos";   
+ let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+     axios.get(apiUrl).then(handleResponse);
+     return <h4> Loading temperature for {weatherData.city}...</h4>;
+}
+}
