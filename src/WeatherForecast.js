a83fdb32c54d705fbc8b8f47ed8f27225f749a16
Liliana Blattner <lilian14torres@gmail.com>
2021-08-12 15:13:01 -0700

    added a 5th day to forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -21,7 +21,7 @@ export default function WeatherForecast(props){
          <div className="WeatherForecast">
           <div className="row">
               {forecast.map((dailyForecast, index) => {
-                  if (index < 4) {
+                  if (index < 5) {
                       return (
                         <div className="col" key={index}>
                             <WeatherForecastDay data={dailyForecast} />
